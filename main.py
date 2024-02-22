5c70eb7cb06339d72306e5486f17fe2158a34d97
Tomas <tmico@gmail.com>
2024-02-22 16:01:41 +0000

    added background image to listing

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
+import os
 from flask import Flask, render_template, request, url_for, redirect, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, SelectField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField, IntegerField, SelectField, FileField
+from wtforms.validators import DataRequired, URL, Regexp
 from flask_ckeditor import CKEditor, CKEditorField
 # from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask.json import jsonify
+
+
+UPLOAD_FOLDER = 'static/assets/img/house_images'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 Bootstrap5(app)
 ckeditor = CKEditor(app)
 
@@ -20,6 +31,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///listings.db'
 db = SQLAlchemy()
 db.init_app(app)
 app.app_context().push()
+
+
 
 
 # #CREATE TABLE IN DB
@@ -32,11 +45,12 @@ class Listings(db.Model):
     link = db.Column(db.String(250), unique=True, nullable=False)
     description = db.Column(db.Text, nullable=False)
     price = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String, nullable=False)
 
 
 # Line below only required once, when creating DB.
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
 
 class CreateListingForm(FlaskForm):
@@ -47,7 +61,19 @@ class CreateListingForm(FlaskForm):
     description = CKEditorField("Listing description", validators=[DataRequired()])
     link = StringField("URL", validators=[DataRequired(), URL()])
     price = IntegerField("Price", validators=[DataRequired()])
+    image = FileField("Image", validators=[DataRequired()])
     submit = SubmitField("Submit Listing")
+
+
+def bad_request(message):
+    response = jsonify({'message': message})
+    response.status_code = 400
+    return response
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
@@ -61,6 +87,13 @@ def get_all_listings():
 def add_listing():
     form = CreateListingForm()
     if form.validate_on_submit():
+
+        image_file=form.image.data
+
+        if image_file and allowed_file(image_file.filename):
+            filename = secure_filename(image_file.filename)
+            image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
         listing = Listings(
             address=form.address.data,
             type=form.type.data,
@@ -68,9 +101,11 @@ def add_listing():
             baths=form.baths.data,
             description=form.description.data,
             link=form.link.data,
-            price=form.price.data)
+            price=form.price.data,
+            image_url=f"static/assets/img/house_images/{form.address.data}")
         db.session.add(listing)
         db.session.commit()
+
         return redirect(url_for('get_all_listings'))
     return render_template("add.html", form=form)
 
